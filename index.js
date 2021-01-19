c8368343600e16fc834e058de9bcc64a6da1e325
buxuku <buxuku@gmail.com>
2021-01-19 21:41:10 +0800

    feat: use nunjucks to render readme

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const fs = require('fs');
 const axios = require('axios');
+const nunjucks = require('nunjucks');
 
 const URL = "http://blog.linxiaodong.com/latest.json";
-const START_TAG = `<!-- POST-START -->`;
-const END_TAG = `<!-- POST-END -->`;
 
 async function build() {
     const {data = []} = await axios.get(URL);
-    const readme = fs.readFileSync("./README.md", "utf8");
-    const indexStart = readme.indexOf(START_TAG) + START_TAG.length;
-    const indexEnd = readme.indexOf(END_TAG);
-    const startContent = readme.substring(0, indexStart);
-    const endContent = readme.substring(indexEnd);
-
-    const postList = data.map(item => `- [${item.title}](https://blog.linxiaodong.com${item.path})`).join('\n');
-    const readmeNew = `
-${startContent}
-${postList}
-${endContent}
-`;
-
-    fs.writeFileSync("./README.md", readmeNew.trim());
+    const temp = nunjucks.render('./readme.njk', {list: data});
+    fs.writeFileSync("./README.md", temp);
 }
 
-try{
+try {
     build()
-}catch(e){
-    console.log('error',e );
+} catch (e) {
+    console.log('error', e);
 }
