c69496305cb07bdf46314628c31cbac390bdd304
buxuku <buxuku@gmail.com>
2021-01-29 16:33:43 +0800

    feat: issue2readme

---
+++ issue.js
@@ -0,0 +1,23 @@
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+const GithubApi = require('./utils/githubApi');
+
+async function build() {
+    const githubApi = new GithubApi({
+        owner: 'buxuku',
+        repo: 'github-api',
+        auth: process.env.GH_TOKEN,
+    });
+    githubApi.init();
+    console.log('fetching',);
+    let data = await githubApi.fetchPageIssues({per_page: 10, page: 1});
+    data = data.map(issue => ({title: issue.title, html_url: issue.html_url}));
+    const temp = nunjucks.render('./readme.njk', {list: data});
+    fs.writeFileSync("./README.md", temp);
+}
+
+try {
+    build()
+} catch (err) {
+    console.log('err', err);
+}
