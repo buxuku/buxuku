f26b3b901339a252874852ce3bbddada71f15a3a
buxuku <buxuku@gmail.com>
2021-01-29 17:00:33 +0800

    feat: change repo

--- issue.js
+++ issue.js
@@ -5,11 +5,11 @@ const GithubApi = require('./utils/githubApi');
 async function build() {
     const githubApi = new GithubApi({
         owner: 'buxuku',
-        repo: 'github-api',
+        repo: 'buxuku.github.io',
         auth: process.env.GH_TOKEN,
     });
     githubApi.init();
-    console.log('fetching',);
+    console.log('fetching');
     let data = await githubApi.fetchPageIssues({per_page: 10, page: 1});
     data = data.map(issue => ({title: issue.title, html_url: issue.html_url}));
     const temp = nunjucks.render('./readme.njk', {list: data});
